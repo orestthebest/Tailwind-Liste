bc18f78ae19880ae2f2d1b649f52af7525d1be50
Orest <bashiorest4@gmail.com>
2026-04-29 11:05:27 +0200

    Added table structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script></script>
+
+
+<div class="flex justify-between items-center">
+  <div class="flex flex-col">
+    <h1 class="font-medium text-2xl">User Accounts</h1>
+    <p class="text-gray-600">View Accounts of registered users</p>
+  </div>
+
+  <button class="flex text-white bg-blue-600 hover:bg-blue-800 rounded-2xl px-6 py-2"> CSV </button>
+</div>
+
+
+<table class="border border-gray-400">
+    <tbody>
+      <tr>
+        <td class="border-t border-b bg-blue-500 px-4 py-2">ID</td>
+        <td class="border-t border-b bg-blue-500 px-4 py-2">FULL NAME</td>
+        <td class="border-t border-b bg-blue-500 px-4 py-2">USER ROLE</td>
+        <td class="border-t border-b bg-blue-500 px-4 py-2">CREATE AT</td>
+        <td class="border-t border-b bg-blue-500 px-4 py-2">STATUS</td>
+      </tr>
+
+      <tr>
+        <td class="border-t border-b px-4 py-2">3</td>
+        <td class="border-t border-b px-4 py-2">Besique Monroe</td>
+        <td class="border-t border-b px-4 py-2">Administrator</td>
+        <td class="border-t border-b px-4 py-2">Sep 28, 2022</td>
+        <td class="border-t border-b px-4 py-2">Active</td>
+      </tr>
+      <tr>
+        <td class="border-t border-b px-4 py-2">7</td>
+        <td class="border-t border-b px-4 py-2">James Carter</td>
+        <td class="border px-4 py-2">Author</td>
+        <td class="border px-4 py-2">Sep 28, 2022</td>
+        <td class="border px-4 py-2">Active</td>
+      </tr>
+
+      <tr>
+        <td class="border px-4 py-2">12</td>
+        <td class="border px-4 py-2">Elvis Son</td>
+        <td class="border px-4 py-2">Editor</td>
+        <td class="border px-4 py-2">Sep 28, 2022</td>
+        <td class="border px-4 py-2">Suspended</td>
+      </tr>
+
+      <tr>
+        <td class="border px-4 py-2">66</td>
+        <td class="border px-4 py-2">Dana White</td>
+        <td class="border px-4 py-2">Administrator</td>
+        <td class="border px-4 py-2">Sep 28, 2022</td>
+        <td class="border px-4 py-2">Inactive</td>
+      </tr>
+    </tbody>
+  </table>
